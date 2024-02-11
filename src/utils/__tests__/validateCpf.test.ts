1346a72ad96ddcbcd40fd5d0ae6beb832356e1dd
Bruno Birck <brunobirck@Brunos-MacBook-Air.local>
2024-02-11 17:58:10 -0300

    feat: create unit tests for utils

---
+++ src/utils/__tests__/validateCpf.test.ts
@@ -0,0 +1,23 @@
+import {formatCPF, isValidCpf} from '@utils/validateCpf';
+
+describe('validateCpf', () => {
+  describe('isValidCpf', () => {
+    it('should return true for a valid CPF', () => {
+      expect(isValidCpf('529.982.247-25')).toBe(true);
+    });
+
+    it('should return false for an invalid CPF', () => {
+      expect(isValidCpf('123.456.789-10')).toBe(false);
+    });
+  });
+
+  describe('formatCPF', () => {
+    it('should correctly format a CPF', () => {
+      expect(formatCPF('52998224725')).toBe('529.982.247-25');
+    });
+
+    it('should correctly format a CPF with extra digits', () => {
+      expect(formatCPF('52998224725123')).toBe('529.982.247-25');
+    });
+  });
+});
